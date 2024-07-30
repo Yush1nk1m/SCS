e04012ec0355b50b08e51ee985fdfd5e722a9701
Yush1nk1m <kys010306@sogang.ac.kr>
2024-07-30 19:52:14 +0900

    Init: Postgres DB 연결 설정
    다른 컨테이너에서 실행 중인 Postgres DB를 연결하기 위해 같은 네트워크에 연결하고 환경 변수를 설정해 다시 빌드하였다.

--- scs-backend/.eslintrc.js
+++ scs-backend/.eslintrc.js
@@ -1,25 +1,28 @@
 module.exports = {
+  env: {
+      browser: true,
+      es2021: true,
+      node: true,
+  },
+  extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended', // Prettier 규칙을 ESLint에 통합
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
+      ecmaVersion: 12,
+      sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
+  plugins: ['@typescript-eslint', 'prettier'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+      'prettier/prettier': [
+          'error',
+          {
+              tabWidth: 4,
+              singleQuote: false,
+          },
+      ],
+      // 추가적인 ESLint 규칙을 여기에 정의할 수 있습니다.
   },
 };
--- scs-backend/src/app.module.ts
+++ scs-backend/src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { typeORMConfig } from "./config/typeorm.config";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [TypeOrmModule.forRoot(typeORMConfig)],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
