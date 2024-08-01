f10f2f68079cca4223b861a67251984818186678
Yush1nk1m <kys010306@sogang.ac.kr>
2024-08-02 00:41:50 +0900

    Test: 단위 테스트 작성
    [R-A-02] 리포지토리에 대한 단위 테스트를 작성하였다.

--- scs-backend/src/auth/auth.repository.spec.ts
+++ scs-backend/src/auth/auth.repository.spec.ts
@@ -9,6 +9,10 @@ describe("AuthRepository", () => {
     let authRepository: AuthRepository;
     let repository: Repository<Verification>;
 
+    // define a mocked input
+    const email: string = "kys010306@sogang.ac.kr";
+    const verificationCode: string = "123456";
+
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             providers: [
@@ -32,10 +36,6 @@ describe("AuthRepository", () => {
     });
 
     describe("[R-A-01] AuthRepository.createVerification()", () => {
-        // define a mocked input
-        const email: string = "kys010306@sogang.ac.kr";
-        const verificationCode: string = "123456";
-
         it("[R-A-01-01] Success", async () => {
             // mock a return value
             const mockedFunc = jest
@@ -68,6 +68,106 @@ describe("AuthRepository", () => {
 
             // check if the mocked function has been called
             expect(mockedFunc).toHaveBeenCalledTimes(1);
+        });
+    });
+
+    describe("[R-A-02] AuthRepository.verify()", () => {
+        // define mocked input
+        const nonExistingVerification: Verification = null;
+        const existingVerification: Verification = {
+            id: 1,
+            email: "kys010306@sogang.ac.kr",
+            verificationCode: "123456",
+            verified: false,
+            createdAt: new Date(),
+        };
+
+        it("[R-A-02-01] Success", async () => {
+            // mock return values
+            const mockedFuncs = [];
+            mockedFuncs.push(
+                jest
+                    .spyOn(repository, "findOne")
+                    .mockResolvedValueOnce(existingVerification),
+            );
+            mockedFuncs.push(
+                jest.spyOn(repository, "save").mockResolvedValueOnce(null),
+            );
+
+            // execute
+            await expect(
+                authRepository.verify(email, verificationCode),
+            ).resolves.toBeTruthy();
+
+            // check if the mocked functions have been called
+            for (const func of mockedFuncs) {
+                expect(func).toHaveBeenCalledTimes(1);
+            }
+        });
+
+        it("[R-A-02-02] Not verified", async () => {
+            // mock return values
+            const mockedFuncs = [];
+            mockedFuncs.push(
+                jest
+                    .spyOn(repository, "findOne")
+                    .mockResolvedValueOnce(nonExistingVerification),
+            );
+
+            // execute
+            await expect(
+                authRepository.verify(email, verificationCode),
+            ).resolves.toBeFalsy();
+
+            // check if the mocked functions have been called
+            for (const func of mockedFuncs) {
+                expect(func).toHaveBeenCalledTimes(1);
+            }
+        });
+
+        it("[R-A-02-03] Exception occurred while finding", async () => {
+            // mock return values
+            const mockedFuncs = [];
+            mockedFuncs.push(
+                jest
+                    .spyOn(repository, "findOne")
+                    .mockRejectedValueOnce(new InternalServerErrorException()),
+            );
+
+            // execute
+            await expect(
+                authRepository.verify(email, verificationCode),
+            ).rejects.toThrow(InternalServerErrorException);
+
+            // check if the mocked functions have been called
+            for (const func of mockedFuncs) {
+                expect(func).toHaveBeenCalledTimes(1);
+            }
+        });
+
+        it("[R-A-02-04] Exception occurred while saving", async () => {
+            // mock return values
+            const mockedFuncs = [];
+            mockedFuncs.push(
+                jest
+                    .spyOn(repository, "findOne")
+                    .mockResolvedValueOnce(existingVerification),
+            );
+            mockedFuncs.push(
+                jest
+                    .spyOn(repository, "save")
+                    .mockRejectedValueOnce(new InternalServerErrorException()),
+            );
+
+            // execute
+            await expect(
+                authRepository.verify(email, verificationCode),
+            ).rejects.toThrow(InternalServerErrorException);
+
+            // check if the mocked functions have been called
+            for (const func of mockedFuncs) {
+                expect(func).toHaveBeenCalledTimes(1);
+            }
         });
     });
 });
